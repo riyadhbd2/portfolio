54b6fb5ee985e70afa72ea1fdcd4474c73c49a60
riyadhbd2 <riyadhbd2@gmail.com>
2025-02-22 01:40:05 +0100

    footer text edited

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,13 +35,13 @@ export default function Projects() {
       </div>
 
         {/* Filter Buttons */}
-        <div className="flex justify-center gap-4 mb-6 mt-3">
+        <div className="flex justify-center gap-4 mb-6 mt-5">
           {categories.map((category) => (
             <button
               key={category}
               className={`px-4 py-2 rounded-md ${
                 filter === category
-                  ? "bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 py-2 px-5 rounded-full text-white"
+                  ? "bg-violet-900 py-2 px-5 rounded-full text-white"
                   : "bg-gray-200 text-gray-700"
               }`}
               onClick={() => setFilter(category)}
